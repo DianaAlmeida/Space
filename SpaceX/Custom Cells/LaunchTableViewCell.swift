5bac1c8b702d610d75ca1aa1b46eb655422e6468
Diana Almeida <dsgalmeida@gmail.com>
2022-10-23 18:20:25 +0100

    *Change -  LaunchTableViewCell

--- SpaceX/Custom Cells/LaunchTableViewCell.swift
+++ SpaceX/Custom Cells/LaunchTableViewCell.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class LaunchTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var missionImage: UIImageView!
+    @IBOutlet weak var missionNameLabel: UILabel!
+    @IBOutlet weak var dateMissionLabel: UILabel!
+    @IBOutlet weak var rocketNameLabel: UILabel!
+    @IBOutlet weak var DaysOfMissionLabel: UILabel!
+    @IBOutlet weak var lauchSuccessfulImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
